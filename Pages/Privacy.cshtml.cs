195aae61c381a9ccd44faa33fafd339d3d1c623a
Otávio Silva <otaviosilva2632@gmail.com>
2021-10-26 21:40:16 -0300

    feat: remove unused imports

--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace QrWeb.Pages
 {
-    public class PrivacyModel : PageModel
+  public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
 
